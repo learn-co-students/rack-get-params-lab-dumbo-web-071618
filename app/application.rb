69a2517e93a3f0a710d9113864846b0fee6deac4
github username <github email address>
2018-08-06 12:20:41 -0400

    Add cart feature

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -13,6 +14,21 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      resp.write "<h1>Cart</h1>"
+      resp.write "<ul>"
+      @@cart.each do |item|
+        resp.write "<li>#{item}</li>"
+      end
+      resp.write "</ul>"
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      if @@items.include?(item)
+        @@cart << item
+        resp.write "Item Added"
+      else
+        resp.write "Couldn't Find Item"
+      end
     else
       resp.write "Path Not Found"
     end
